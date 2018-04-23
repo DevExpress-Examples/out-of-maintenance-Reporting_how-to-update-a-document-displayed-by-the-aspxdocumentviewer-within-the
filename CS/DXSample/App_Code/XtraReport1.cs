08d44992c40109ffe284af2b892a7d5871e8fe28
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXSample/App_Code/XtraReport1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+/// <summary>
+/// Summary description for XtraReport1
+/// </summary>
+public class XtraReport1 : DevExpress.XtraReports.UI.XtraReport
+{
+    private DevExpress.XtraReports.UI.DetailBand Detail;
+    private XRLabel xrLabel1;
+    private DevExpress.XtraReports.Parameters.Parameter test;
+	/// <summary>
+	/// Required designer variable.
+	/// </summary>
+	private System.ComponentModel.IContainer components = null;
+
+	public XtraReport1()
+	{
+		InitializeComponent();
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+	
+	/// <summary> 
+	/// Clean up any resources being used.
+	/// </summary>
+	/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	protected override void Dispose(bool disposing) {
+		if (disposing && (components != null)) {
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	#region Designer generated code
+
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent() {
+        string resourceFileName = "XtraReport1.resx";
+        this.Detail = new DevExpress.XtraReports.UI.DetailBand();
+        this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
+        this.test = new DevExpress.XtraReports.Parameters.Parameter();
+        ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+        // 
+        // Detail
+        // 
+        this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.xrLabel1});
+        this.Detail.Height = 39;
+        this.Detail.Name = "Detail";
+        this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F);
+        this.Detail.RepeatCountOnEmptyDataSource = 20;
+        this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+        // 
+        // xrLabel1
+        // 
+        this.xrLabel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+        this.xrLabel1.Location = new System.Drawing.Point(17, 0);
+        this.xrLabel1.Name = "xrLabel1";
+        this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F);
+        this.xrLabel1.Size = new System.Drawing.Size(525, 25);
+        this.xrLabel1.StylePriority.UseFont = false;
+        this.xrLabel1.Text = "Report Content";
+        // 
+        // test
+        // 
+        this.test.Name = "test";
+        this.test.Value = "";
+        // 
+        // XtraReport1
+        // 
+        this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.Detail});
+        this.Margins = new System.Drawing.Printing.Margins(0, 0, 100, 100);
+        this.PageHeight = 400;
+        this.PageWidth = 600;
+        this.PaperKind = System.Drawing.Printing.PaperKind.Custom;
+        this.Parameters.AddRange(new DevExpress.XtraReports.Parameters.Parameter[] {
+            this.test});
+        this.Version = "8.2";
+        this.BeforePrint += new System.Drawing.Printing.PrintEventHandler(this.XtraReport1_BeforePrint);
+        ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+	}
+
+	#endregion
+
+    private void XtraReport1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e) {
+        this.xrLabel1.Text += " -- " + this.Parameters["test"].Value.ToString();
+    }
+}
---
+++ VB/DXSample/Default.aspx.vb
@@ -0,0 +1,27 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxPopupControl
+Imports DevExpress.Web.ASPxCallbackPanel
+Imports DevExpress.XtraReports.Web
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim test As String = String.Empty
+		Dim hf As HtmlInputHidden = CType(Me.Page.FindControl("ReportMsg"), HtmlInputHidden)
+		Dim report As New XtraReport1()
+		report.Parameters("test").Value = hf.Value.ToString()
+		report.CreateDocument()
+		Me.DocumentViewer1.Report = report
+		Me.DocumentViewer1.DataBind()
+	End Sub
+End Class
