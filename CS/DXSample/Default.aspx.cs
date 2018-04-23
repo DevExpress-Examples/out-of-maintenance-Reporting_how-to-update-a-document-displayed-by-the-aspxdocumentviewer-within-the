19542269aef9b30a01d5ed5c3725b9ba42ddd6d2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXSample/Default.aspx.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Web.ASPxPopupControl;
+using DevExpress.Web.ASPxCallbackPanel;
+using DevExpress.XtraReports.Web;
+
+public partial class _Default : System.Web.UI.Page 
+{
+
+    protected void Page_Load(object sender, EventArgs e) {
+        string test = string.Empty;
+
+        HtmlInputHidden hf = (HtmlInputHidden)this.Page.FindControl("ReportMsg");
+        XtraReport1 report = new XtraReport1();
+        report.Parameters["test"].Value = hf.Value.ToString();
+        report.CreateDocument();
+        this.ReportViewer1.Report = report;
+        this.ReportViewer1.DataBind();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to update a document displayed by the ASPxDocumentViewer within the ASPxPopupControl
+
+
+<p>This example illustrates how to update a web report's content on the fly by placing the <a href="http://documentation.devexpress.com/#XtraReports/CustomDocument5193"><u>ASPxDocumentViewer</u></a> within the <a href="http://documentation.devexpress.com/#AspNet/CustomDocument8289"><u>ASPxPopupControl</u></a> and calling the <strong>ASPxClient</strong><strong>Document</strong><strong>Viewer.Refresh</strong> method.</p><p>An additional hidden field is provided on the web page to pass parameter values to a report before its document is set forth and published using the <strong>ASPxDocumentViewer</strong>.</p>
+
+<br/>
+
+
