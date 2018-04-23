d83717c5f25f7c81060de89e1678a94a6255247d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXSample/App_Code/XtraReport1.vb
@@ -0,0 +1,112 @@
+﻿' Developer Express Code Central Example:
+' How to update a document displayed by the ASPxDocumentViewer within the ASPxPopupControl
+' 
+' This example illustrates how to update a web report's content on the fly by
+' placing the ASPxDocumentViewer
+' (http://documentation.devexpress.com/#XtraReports/CustomDocument5193) within the
+' ASPxPopupControl
+' (http://documentation.devexpress.com/#AspNet/CustomDocument8289) and calling the
+' ASPxClientDocumentViewer.Refresh method.
+' An additional hidden field is provided
+' on the web page to pass parameter values to a report before its document is set
+' forth and published using the ASPxDocumentViewer.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E914
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports DevExpress.XtraReports.UI
+
+''' <summary>
+''' Summary description for XtraReport1
+''' </summary>
+Public Class XtraReport1
+	Inherits DevExpress.XtraReports.UI.XtraReport
+	Private Detail As DevExpress.XtraReports.UI.DetailBand
+	Private xrLabel1 As XRLabel
+	Private test As DevExpress.XtraReports.Parameters.Parameter
+	''' <summary>
+	''' Required designer variable.
+	''' </summary>
+	Private components As System.ComponentModel.IContainer = Nothing
+
+	Public Sub New()
+		InitializeComponent()
+		'
+		' TODO: Add constructor logic here
+		'
+	End Sub
+
+	''' <summary> 
+	''' Clean up any resources being used.
+	''' </summary>
+	''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	Protected Overrides Overloads Sub Dispose(ByVal disposing As Boolean)
+		If disposing AndAlso (components IsNot Nothing) Then
+			components.Dispose()
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+
+	#Region "Designer generated code"
+
+	''' <summary>
+	''' Required method for Designer support - do not modify
+	''' the contents of this method with the code editor.
+	''' </summary>
+	Private Sub InitializeComponent()
+		Dim resourceFileName As String = "XtraReport1.resx"
+		Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+		Me.xrLabel1 = New DevExpress.XtraReports.UI.XRLabel()
+		Me.test = New DevExpress.XtraReports.Parameters.Parameter()
+		CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+		' 
+		' Detail
+		' 
+		Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrLabel1})
+		Me.Detail.Height = 39
+		Me.Detail.Name = "Detail"
+		Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+		Me.Detail.RepeatCountOnEmptyDataSource = 20
+		Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+		' 
+		' xrLabel1
+		' 
+		Me.xrLabel1.Font = New System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, (CByte(204)))
+		Me.xrLabel1.Location = New System.Drawing.Point(17, 0)
+		Me.xrLabel1.Name = "xrLabel1"
+		Me.xrLabel1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+		Me.xrLabel1.Size = New System.Drawing.Size(525, 25)
+		Me.xrLabel1.StylePriority.UseFont = False
+		Me.xrLabel1.Text = "Report Content"
+		' 
+		' test
+		' 
+		Me.test.Name = "test"
+		Me.test.Value = ""
+		' 
+		' XtraReport1
+		' 
+		Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail})
+		Me.Margins = New System.Drawing.Printing.Margins(0, 0, 100, 100)
+		Me.PageHeight = 400
+		Me.PageWidth = 600
+		Me.PaperKind = System.Drawing.Printing.PaperKind.Custom
+		Me.Parameters.AddRange(New DevExpress.XtraReports.Parameters.Parameter() { Me.test})
+		Me.Version = "8.2"
+'		Me.BeforePrint += New System.Drawing.Printing.PrintEventHandler(Me.XtraReport1_BeforePrint);
+		CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+	End Sub
+
+	#End Region
+
+	Private Sub XtraReport1_BeforePrint(ByVal sender As Object, ByVal e As System.Drawing.Printing.PrintEventArgs) Handles MyBase.BeforePrint
+		Me.xrLabel1.Text &= " -- " & Me.Parameters("test").Value.ToString()
+	End Sub
+End Class
---
+++ VB/DXSample/Default.aspx.vb
@@ -0,0 +1,44 @@
+﻿' Developer Express Code Central Example:
+' How to update a document displayed by the ASPxDocumentViewer within the ASPxPopupControl
+' 
+' This example illustrates how to update a web report's content on the fly by
+' placing the ASPxDocumentViewer
+' (http://documentation.devexpress.com/#XtraReports/CustomDocument5193) within the
+' ASPxPopupControl
+' (http://documentation.devexpress.com/#AspNet/CustomDocument8289) and calling the
+' ASPxClientDocumentViewer.Refresh method.
+' An additional hidden field is provided
+' on the web page to pass parameter values to a report before its document is set
+' forth and published using the ASPxDocumentViewer.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E914
+
+
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxPopupControl
+Imports DevExpress.Web.ASPxCallbackPanel
+Imports DevExpress.XtraReports.Web
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim test As String = String.Empty
+		Dim hf As HtmlInputHidden = CType(Me.Page.FindControl("ReportMsg"), HtmlInputHidden)
+		Dim report As New XtraReport1()
+		report.Parameters("test").Value = hf.Value.ToString()
+		report.CreateDocument()
+		Me.DocumentViewer1.Report = report
+		Me.DocumentViewer1.DataBind()
+	End Sub
+End Class
